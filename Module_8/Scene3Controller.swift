9e389ac47ebac1f159f2907e34f88dc8c155d16e
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2021-01-18 13:57:21 +0300

    Task 3 was completed. The module is ready

---
+++ Module_8/Scene3Controller.swift
@@ -0,0 +1,51 @@
+//
+//  Scene3Controller.swift
+//  Module_8
+//
+//  Created by Любовь Волкова on 18.01.2021.
+//
+
+import UIKit
+
+class Scene3Controller: UIViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet var textfields: [UITextField]!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setViewBackgound(color: .green)
+        for button in buttons {
+            button.layer.cornerRadius = 10
+        }
+    }
+    
+    func setViewBackgound(color: UIColor) {
+        self.view.backgroundColor = color
+    }
+    
+    func setHiddenProps(textfieldBool: Bool, buttonBool: Bool, imageBool: Bool) {
+        for i in 0..<2 {
+            textfields[i].isHidden = textfieldBool
+            buttons[i].isHidden = buttonBool
+        }
+        imageView.isHidden = imageBool
+    }
+    
+    @IBAction func changeView(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.setViewBackgound(color: .green)
+            self.setHiddenProps(textfieldBool: false, buttonBool: true, imageBool: true)
+        case 1:
+            self.setViewBackgound(color: .blue)
+            self.setHiddenProps(textfieldBool: true, buttonBool: false, imageBool: true)
+        case 2:
+            self.setViewBackgound(color: .purple)
+            self.setHiddenProps(textfieldBool: true, buttonBool: true, imageBool: false)
+        default:
+            break
+        }
+    }
+}
