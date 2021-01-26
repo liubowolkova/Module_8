707b8be15286ce33e4b2c5164788e4ac4bff6bf4
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2021-01-26 16:06:14 +0300

    Item 2 was updated

--- Module_8/Scene2Controller.swift
+++ Module_8/Scene2Controller.swift
@@ -8,34 +8,59 @@
 import UIKit
 
 class Scene2Controller: UIViewController {
+    // Number of elements in gallery
+    let numberOfElements = 4
+    
+    // Elements sizes
+    let imgWidth = 185
+    let imgHeight = 329
+    let labelWidth = 185
+    let labelHeight = 20
+    
+    // Elements margins
+    let marginLeftFirstColumn = 10
+    let marginLeftSecondColumn = 215
+    
+    // Y-axis distance between elements
+    let distance = 15
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createGallery(self.numberOfElements)
+    }
+    
+    private func setImageFrame(x: Int, y: Int) -> CGRect {
+        return CGRect(x: x, y: y, width: self.imgWidth, height: self.imgHeight)
+    }
+    
+    private func setLabelFrame(x: Int, y: Int) -> CGRect {
+        return CGRect(x: x, y: y, width: self.labelWidth, height: self.labelHeight)
+    }
+    
+    private func createGallery(_ numberOfElements: Int) {
+        var marginLeft = 0
+        var marginTop = 40
         
-        let imageFrames = [
-            1: CGRect(x: 10, y: 40, width: 185, height: 329),
-            2: CGRect(x: 215, y: 40, width: 185, height: 329),
-            3: CGRect(x: 10, y: 419, width: 185, height: 329),
-            4: CGRect(x: 215, y: 419, width: 185, height: 329)
-        ]
-        
-        let labelFrames = [
-            1: CGRect(x: 10, y: 374, width: 185, height: 20),
-            2: CGRect(x: 215, y: 374, width: 185, height: 20),
-            3: CGRect(x: 10, y: 753, width: 185, height: 20),
-            4: CGRect(x: 215, y: 753, width: 185, height: 20)
-        ]
-        
-        for i in 1...4 {
-            let imageView = UIImageView(image: UIImage(named: "Pic_\(i).png"))
-            imageView.frame = imageFrames[i]!
-            self.view.addSubview(imageView)
+        for i in 1...numberOfElements {
+            let thisMarginTop = marginTop
+            marginLeft = i.isMultiple(of: 2) ? self.marginLeftSecondColumn : self.marginLeftFirstColumn
             
-            let label = UILabel(frame: labelFrames[i]!)
+            let imageView = UIImageView(image: UIImage(named: "Pic_\(i).png"))
+            imageView.frame = self.setImageFrame(x: marginLeft, y: marginTop)
+            
+            marginTop += self.imgHeight + self.distance
+            
+            let label = UILabel(frame: self.setLabelFrame(x: marginLeft, y: marginTop))
             label.text = "Image \(i)"
             label.textAlignment = .left
             label.tintColor = .black
+            
+            self.view.addSubview(imageView)
             self.view.addSubview(label)
+            
+            marginTop += self.labelHeight + self.distance
+            
+            marginTop = i.isMultiple(of: 2) ? marginTop : thisMarginTop
         }
     }
 }
